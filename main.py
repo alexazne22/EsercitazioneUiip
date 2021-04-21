2ba54f4d8108497fd521dc835276104809a97022
Alessandra <zanellialessandra@gmail.com>
2021-04-21 15:10:25 +0200

    Funzione 4: operativa

--- main.py
+++ main.py
@@ -14,14 +14,19 @@ lista_test = [ Auto('x', 10, 5, 'y', 'u'), Auto('a', 5, 50, 'b', 'c')]
 while comando != -1:
     if comando == 1: #Lettura
         print()
-        print()
+
     if comando == 2: #Ricerca
         print()
     if comando == 3:#Occorrenza di un produttore
         print()
     if comando == 4:#Modello più cavalli
-
-        print()
+        cavalli_max = 0
+        modello_max_cavalli = ''
+        for c in lista_test:
+            if c.cavalli > cavalli_max:
+                cavalli_max = c.cavalli
+                modello_max_cavalli = c.modello
+        print('Il modello con più cavalli è ' + modello_max_cavalli)
     if comando == 5: #Media delle cilindrate
         print()
     comando = input('Inserisci comando: ')
